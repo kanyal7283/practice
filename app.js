0e457fbe024a7d2147ea7560885b2ae62618b90a
kanyal7283 <deepakkanyal1305@gmail.com>
2025-09-19 09:28:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+//const authRoutes = require('./routes/authRoutes');
+
+
+// Middleware to set security headers
+  app.use((req, res, next) => {
+  res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('X-XSS-Protection', '1; mode=block');
+  res.setHeader('Content-Security-Policy', "default-src 'self' https://hmsi-portal-uat-48801d68a3ef.herokuapp.com");
+  res.setHeader('Date', new Date().toUTCString());
+  res.setHeader('Content-Length', Buffer.byteLength(JSON.stringify(res.body || ''))); 
+  res.setHeader('Via', '1.1 some-proxy-server');
+  res.setHeader('Connection', 'keep-alive');
+  res.setHeader('X-Powered-By', 'Express');
+  res.setHeader('Cache-Control', 'no-store, max-age=0');
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+  next();
+});
+
+
+// Example of logging an API request
+app.get('/', (req, res) => {
+  res.send('Welcome to service invoice api ------first---->.');
+});
+
+// Use the auth routes
+//app.use('/api', authRoutes);
+
+// Start the server
+const PORT = (process.env.PORT ?? 9000);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ------> ${PORT}`);
+});
+
